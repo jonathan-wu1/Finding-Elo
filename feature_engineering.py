bc42898c362a66f4d78c9314d6be2aef4e2ba2e9
jonathan-wu1 <jonathanwu1818@gmail.com>
2024-08-28 17:49:24 +1000

    more feature extractions

--- feature_engineering.py
+++ feature_engineering.py
@@ -160,7 +160,12 @@ def process_pgn_file(pgn_file_path, limit):
 
     return games
 
-
+def save_results(result, start_index):
+    ndjson_file_path = f'data/raw/feature_engineered_chunk_{start_index}.ndjson'
+    with open(ndjson_file_path, 'w') as f:
+        for item in result:
+            json.dump(item, f)
+            f.write('\n')
 
 if __name__ == "__main__":
 
@@ -170,22 +175,39 @@ if __name__ == "__main__":
     sys.setrecursionlimit(50000)
     start_time = time.time()
     pgn_file_path = "data/data_openings.pgn"
-    games = process_pgn_file(pgn_file_path, 21600)
+    games = process_pgn_file(pgn_file_path, 50000)
     print(f"number of games: {len(games)}")
 
-    with mp.Pool() as pool:
-      result = pool.map(extract_features, games)
-    
-
-    print(result[0])
-    # Save to an NDJSON file
-    ndjson_file_path = 'data/raw/feature_engineered1.ndjson'
-
-    with open(ndjson_file_path, 'w') as f:
-        for item in result:
-            json.dump(item, f)
-            f.write('\n')  # Write a newline after each JSON object
-
     end_time = time.time()
-    print(end_time - start_time)
+    print(f"Reading time: {end_time - start_time}")
+
+    start_time = time.time()
+    chunk_size = 5000  # Process 1000 games at a time
+    num_chunks = len(games) // chunk_size + (1 if len(games) % chunk_size > 0 else 0)
+
+    for i in range(num_chunks):
+        chunk_start = time.time()
+        print(f"Chunk starting at index {i}")
+        start_index = i * chunk_size
+        end_index = min(start_index + chunk_size, len(games))
+        games_chunk = games[start_index:end_index]
+
+        try:
+            with mp.Pool() as pool:
+                result = pool.map(extract_features, games_chunk)
+            save_results(result, start_index)
+
+            chunk_end = time.time()
+            print(f"Processed and saved chunk {start_index} to {start_index + chunk_size}")
+            print(f"Time taken to process {chunk_size} games: {chunk_end - chunk_start}")
+            print("------------------------------------------")
+        except Exception as e:
+            print(f"An error occurred while processing chunk {start_index}: {e}")
+            
+    
+    end_time = time.time()
+    print(f"Processing time total: {end_time - start_time}")
+
+    
+    
 
